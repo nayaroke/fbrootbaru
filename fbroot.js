2faf2ee30c33fdef0282893cb8b871bf278c6412
nayaroke <ahmadnayar99@gmail.com>
2018-05-09 20:18:51 +0800

    Update fbroot.js

--- fbroot.js
+++ fbroot.js
@@ -2,11 +2,11 @@
 var js, fjs = d.getElementsByTagName(s)[0];
 if (d.getElementById(id)) return;
 js = d.createElement(s); js.id = id;js.async=true;
-js.src = "https//connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v2.12";
+js.src = "//connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v3.0";
 fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 function downloadJSAtOnload() {
         var e = document.createElement("script");
-        e.src = "//connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v2.7", document.body.appendChild(e)
+        e.src = "https//connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v3.0", document.body.appendChild(e)
     }
     window.addEventListener ? window.addEventListener("load", downloadJSAtOnload, !1) : window.attachEvent ? window.attachEvent("onload", downloadJSAtOnload) : window.onload = downloadJSAtOnload;
